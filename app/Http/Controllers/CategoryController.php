dc17ad056f9f8b79fbf5f45592da05ca1d544d5f
Adam M <whiskypo082@gmail.com>
2020-01-17 11:34:41 -0600

    CRUD for Category controller api

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Model\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -14,18 +15,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return Category::latest()->get(); 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -35,7 +27,13 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Category::create9$request->all()); 
+        $category = new Category; 
+        $category->name = $request->name; 
+        $category->slug = Str::slug($request->name); 
+        $category->save(); 
+
+        return response('Created'); 
     }
 
     /**
@@ -46,18 +44,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
+        return $category; 
     }
 
     /**
@@ -69,7 +56,13 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        
+        $category->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ]); 
+
+        return response('UPDATED'); 
     }
 
     /**
@@ -80,6 +73,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete(); 
+        return \response('DELETED'); 
     }
 }
