8a33174f98b2e99e4131ceea08f530533c35a2a0
Adam M <whiskypo082@gmail.com>
2020-01-17 10:53:12 -0600

    CRUD and collections set up

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Question;
 use Illuminate\Http\Request;
+use App\Http\Resources\QuestionResource; 
 
 class QuestionController extends Controller
 {
@@ -14,7 +15,7 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        return QuestionResource::collection(Question::latest()->get());  
     }
 
     /**
@@ -35,7 +36,10 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // auth()->user()->question()->create($request->all());
+       Question::create($request->all()); 
+       return \response('Created'); 
+
     }
 
     /**
@@ -46,7 +50,7 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        return new QuestionResource($question); 
     }
 
     /**
@@ -80,6 +84,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete(); 
+        return \response('Deleted'); 
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $guarded = []; 
+
+    public function getRouteKeyName()
+    {
+        return 'slug'; 
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class); 
@@ -19,5 +26,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class); 
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug"); 
     }
 }
