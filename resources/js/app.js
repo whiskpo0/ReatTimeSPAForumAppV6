e01127f85b4aad84654b7ec747047aceffc6e782
Adam M <whiskypo082@gmail.com>
2020-01-21 15:50:28 -0600

    Added Vuetify and some components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
+Vue.use(Vuetify)
+
+const opts = {}
+ 
+export default new Vuetify(opts)
+ 
+window.Vuetify = require("vuetify");
+
+
+Vue.component('AppHome', require('./components/AppHome.vue').default);
+
+import router from '../js/components/Router/router.js'
+
+const app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(), 
+    router
+});
+
+ 
